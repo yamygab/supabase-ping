f6441040a93f7ef236678c27c6ca40dff4f54d24
yamygab <yami45221@gmail.com>
2025-12-24 02:08:47 +0545

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Oxanium', 'sans-serif'],
+      },
+    },
+  },
+  darkMode: 'class',
+  plugins: [],
+}
